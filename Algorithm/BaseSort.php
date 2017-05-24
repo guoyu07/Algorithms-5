2df57c79ffaf0d855d03a76ffcbc1de34ef58000
Zhang Qiankun <willzhang@hsdc.net.cn>
2017-05-24 13:18:10 +0800

    fist push

---
+++ Algorithm/BaseSort.php
@@ -0,0 +1,41 @@
+<?php
+
+/**
+ * Time: 2017/5/24 上午8:21
+ */
+namespace Algorithm;
+abstract class BaseSort
+{
+    protected $unsorted;
+    protected $sorted;
+
+    const INSERTION = 'Insertion';
+    const SELECTION = 'Selection';
+
+    /**
+     * @param $algorithm
+     * @return BaseSort
+     */
+    public static function getInstance($algorithm)
+    {
+        switch ($algorithm) {
+            case self::INSERTION:
+                return new Insertion();
+                break;
+            case self::SELECTION:
+                return new Selection();
+        }
+    }
+
+    public function setUnsorted($unsorted)
+    {
+        $this->unsorted = $unsorted;
+    }
+
+    public function getSorted()
+    {
+        return $this->sorted;
+    }
+
+    abstract function sort();
+}
---
+++ Algorithm/Selection.php
@@ -0,0 +1,24 @@
+<?php
+namespace Algorithm;
+
+class Selection extends BaseSort
+{
+    function sort()
+    {
+        $total = count($this->unsorted);
+
+        for ( $i = 0; $i < $total; $i++ ) {
+            $min = $i;
+            for ( $j = $i+1; $j < $total; $j++ ) {
+                if ( $this->unsorted[$j] < $this->unsorted[$min] ) $min = $j;
+            }
+
+            // exchange
+            $tmp = $this->unsorted[$i];
+            $this->unsorted[$i] = $this->unsorted[$min];
+            $this->unsorted[$min] = $tmp;
+        }
+
+        $this->sorted = $this->unsorted;
+    }
+}
